3aedfb7e5ad8956b39be474fca1f314036797f22
FutureClouds <aws.futurex@gmail.com>
2026-04-12 10:09:05 +0300

    Add files via upload

--- SAA.js
+++ SAA.js
@@ -2731,7 +2731,7 @@ https://www.examtopics.com/discussions/amazon/view/99659-exam-aws-certified-solu
 Q: A company wants to create a mobile app that allows users to stream slow-motion video clips on their mobile devices. Currently, the app captures video clips and uploads the video clips in raw format into an Amazon S3 bucket. The app retrieves these video clips directly from the S3 bucket. However, the videos are large in their raw format.\nUsers are experiencing issues with buffering and playback on mobile devices. The company wants to implement solutions to maximize the performance and scalability of the app while minimizing operational overhead.\nWhich combination of solutions will meet these requirements? (Choose two.)
 *A. Deploy Amazon CloudFront for content delivery and caching.
 B. Use AWS DataSync to replicate the video files across AW'S Regions in other S3 buckets.
-C. Use Amazon Elastic Transcoder to convert the video files to more appropriate formats.
+*C. Use Amazon Elastic Transcoder to convert the video files to more appropriate formats.
 D. Deploy an Auto Sealing group of Amazon EC2 instances in Local Zones for content delivery and caching.
 E. Deploy an Auto Scaling group of Amazon EC2 instances to convert the video files to more appropriate formats.
 https://www.examtopics.com/discussions/amazon/view/99693-exam-aws-certified-solutions-architect-associate-saa-c03/
