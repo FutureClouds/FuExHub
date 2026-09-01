6349f6461ab4bd83be0be060a995f5aecf71f2f6
FutureClouds <aws.futurex@gmail.com>
2026-09-01 16:39:02 +0300

    Add files via upload

--- AIF.js
+++ AIF.js
@@ -1451,7 +1451,7 @@ https://www.examtopics.com/discussions/amazon/view/304562-exam-aws-certified-ai-
 Q: A publishing company built a Retrieval Augmented Generation (RAG) based solution to give its users the ability to interact with published content. New content is published daily. The company wants to provide a near real-time experience to users.\nWhich steps in the RAG pipeline should the company implement by using offline batch processing to meet these requirements? (Choose two.)
 *A. Generation of content embeddings
 B. Generation of embeddings for user queries
-C. Creation of the search index
+*C. Creation of the search index
 D. Retrieval of relevant content
 E. Response generation for the user
 https://www.examtopics.com/discussions/amazon/view/304552-exam-aws-certified-ai-practitioner-aif-c01-topic-1-question/
